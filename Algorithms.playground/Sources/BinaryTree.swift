0e78210dd8aa1c0386699b218cd202c825d3a3c7
Vladyslav Shkodych <vladyslav.shkodych@jmind.systems>
2023-11-19 16:26:08 +0200

    [add] IsBinaryTreeSymmetric solution to the Algorithms playground

--- Algorithms.playground/Sources/BinaryTree.swift
+++ Algorithms.playground/Sources/BinaryTree.swift
@@ -14,8 +14,8 @@ public enum BinaryTree {
         return makeTree(for: [5, 11, 3, 4, 2, 1])
     }
 
-    public static var numbersTree2: Node<Int> {
-        return makeTree(for: [5, 12, 4, 5, 3, 2])
+    public static var numbersTreeSymmetric: Node<Int> {
+        return makeTree(for: [4, 5, 5, 2, 8, 8, 2, 9, 7, 1, 0, 0, 1,  7, 9])
     }
 
     private static func makeTree<T>(for array: [T]) -> Node<T> {
